3acf576b8e7147db59261e7ea8e03c63c3d79c67
Kiranhackers <kiranhosagoudar1@gmail.com>
2025-12-26 01:37:22 +0530

    Update API request and response handling in task4.py
    Refactor API request handling and response saving.

--- Evaluation_DEC20/task4.py
+++ Evaluation_DEC20/task4.py
@@ -1,38 +1,23 @@
 import requests
 import json
 
-# Your existing credentials and headers
-TOKEN = "Bearer E7sKdN46eeZpdw7h0nhQ.zD79gEr7V7gDALlWMexSNc-1766213936-1.0.1.1-riFXUbo_XmHi2iKluRT6tPWUigmXbS7vYgqPnhrDRhg"
-headers = {
-     "Authorization": f"Bearer {TOKEN}",
- }
+TOKEN="Bearer E7sKdN46eeZpdw7h0nhQ.zD79gEr7V7gDALlWMexSNc-1766213936-1.0.1.1-riFXUbo_XmHi2iKluRT6tPWUigmXbS7vYgqPnhrDRhg"
+headers={
+    "Authorization":f"Bearer {TOKEN}"
+}
 
-url = "https://reqres.in/api/users/2"
-responses = requests.get(url, headers=headers)
+url="https://reqres.in/api/users/2"
 
+p={
+    "name":"kiran",
+    "age":25
+}
 
-data = {
-         "job":"oracle"
-     }
+r=requests.put(url,p,headers=headers)
+if r.status_code==200:
+    data=r.json()
 
-response = requests.post(url, json=data, headers=headers)
-response = requests.put(url, json=data, headers=headers) 
-# Check if the request was successful
-if responses.status_code == 200:
-     data = responses.json()
-    
-#    # Save to a JSON file
-     with open('task4.json', 'w', encoding='utf-8') as f:
-         json.dump(data, f, indent=4)
-    
-     print("Status Code:", responses.status_code)
-     print("Success! Response saved to 'task4.json'")
+    with open('b.json','w')as f:
+        json.dump(data,f,indent=4)
 else:
-     print(f"Error: {responses.status_code}")
-     print(responses.text)
-
-json_data = response.json()
-json_str = json.dumps(json_data, indent=4)
-print("json POST response body: ", json_str)
-
-
+    print("put is not replacing entire resources")
